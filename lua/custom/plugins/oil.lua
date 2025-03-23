62795952f688ef22ecab0d240d3938d2e5dba402
Kfir Fitousi <kfirfitousi@gmail.com>
2025-03-23 15:20:28 +0200

    fix harpoon and oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,12 +2,30 @@ return {
   {
     'stevearc/oil.nvim',
     config = function()
-      require('oil').setup()
+      require('oil').setup {
+
+        keymaps = {
+          ['<C-s>'] = false,
+          ['|'] = { 'actions.select', opts = { vertical = true } },
+        },
+        float = {
+          max_height = 0.5,
+          max_width = 0.5,
+          border = 'rounded',
+        },
+        view_options = {
+          show_hidden = true,
+          is_always_hidden = function(name, bufnr)
+            return name == '.DS_Store'
+          end,
+        },
+      }
+
       vim.keymap.set('n', '\\', function()
         if vim.bo.filetype == 'oil' then
-          vim.cmd 'b#' -- Go back to the last buffer if Oil is open
+          require('oil.actions').close.callback()
         else
-          vim.cmd 'Oil'
+          vim.cmd 'Oil --float'
         end
       end, { desc = 'Toggle Oil or return to last buffer' })
     end,
